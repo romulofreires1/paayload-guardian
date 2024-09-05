8213717bfbae2150ac997df7c0e46123e1e15997
Romulo Freires <romulofreires1@gmail.com>
2024-09-05 18:37:20 -0300

    fix: lint

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -4,5 +4,5 @@ import json
 class ConfigLoader:
     @staticmethod
     def load_config(file_path):
-        with open(file_path, "r") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             return json.load(file)
--- src/infrastructure/fetcher/http_fetcher.py
+++ src/infrastructure/fetcher/http_fetcher.py
@@ -7,9 +7,8 @@ class HttpFetcher:
             response = requests.get(route, timeout=0.5)
             if response.status_code == 200:
                 return response.json()
-            else:
-                print(f"Failed to fetch payload from {route}")
-                return None
+            print(f"Failed to fetch payload from {route}")
+            return None
         except requests.RequestException as e:
             print(f"Error fetching payload from {route}: {e}")
             return None
